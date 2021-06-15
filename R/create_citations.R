b8eba688a456784fad6ba750e2ce6f154eb4d48b
Matthew Wankiewicz <matthew.wankiewicz@hotmail.com>
2021-06-15 13:18:20 -0400

    first commit

---
+++ R/create_citations.R
@@ -0,0 +1,63 @@
+#' create_citations
+#'
+#' This function creates citations for multiple R packages at once. By default, this function will create
+#' a citation for R, along with the other functions you include. When creating the citations, you
+#' can choose to either receive text citations or have them formatted in BibTeX. If you choose to
+#' get BibTeX citations, the function will create a new file called 'references.bib' which will store
+#' the citations. BibTeX is the default formatting argument for create_citations.
+#'
+#' @param packages A list of packages that you have used or plan to use in your document.
+#' @param format The type of citations you want to return. Returns bibtex by default, use `format = text` to return text citations.
+#'
+#' @return A list of citations for the packages included in a document.
+#' @export
+#'
+#' @examples create_citations(packages = list("tidyverse", "DoSStoolkit"),
+#'  format = "bibtex")
+#'
+#' create_citations(list("Lahman", "ggplot2", "forcats"),
+#' "text")
+#'
+#' create_citations(list("tidyverse"))
+#'
+create_citations <- function(packages, format = "bibtex"){
+  if("base" %in% packages){
+    if(format == "bibtex"){
+      bib <- file("references.bib")
+      citations <- list()
+      for(reference in packages){
+        citations <- append(citations, citation(reference))
+      }
+      cites <- as.character(toBibtex(citations))
+      writeLines(cites, bib)
+      close(bib)
+    }
+    if(format == "text"){
+      citations <- list()
+      for(reference in packages){
+        citations <- append(citations, citation(reference))
+      }
+      print(citations)
+    }
+  }
+  else{
+    packages <- append(packages, "base")
+    if(format == "bibtex"){
+      bib <- file("references.bib")
+      citations <- list()
+      for(reference in packages){
+        citations <- append(citations, citation(reference))
+      }
+      cites <- as.character(toBibtex(citations))
+      writeLines(cites, bib)
+      close(bib)
+    }
+    if(format == "text"){
+      citations <- list()
+      for(reference in packages){
+        citations <- append(citations, citation(reference))
+      }
+      print(citations)
+    }
+  }
+}
