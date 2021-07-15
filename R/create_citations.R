0565eb19525d8efcdb59bc83eeeee4daf3013b80
Matthew Wankiewicz <matthew.wankiewicz@hotmail.com>
2021-07-15 15:09:35 -0400

    reworked functions

--- R/create_citations.R
+++ R/create_citations.R
@@ -5,7 +5,7 @@
 #' can choose to either receive text citations or have them formatted in BibTeX. The function will return
 #' citations in the console pane in your RStudio window.
 #'
-#' @param packages A list of packages that you have used or plan to use in your document.
+#' @param packages A list of packages that you have used or plan to use in your document. If packages argument is empty, the function will create citations for packages loaded into current R session.
 #' @param format The type of citations you want to return. Returns bibtex by default, use `format = text` to return text citations.
 #' @param include_r If set to TRUE, will create a citation for R.
 #'
@@ -20,39 +20,78 @@
 #'
 #' create_citations(list("tidyverse"))
 #'
-create_citations <- function(packages, format = "bibtex",
+create_citations <- function(packages = NULL, format = "bibtex",
                              include_r = TRUE){
-  suppressWarnings(if(include_r == FALSE){
-    if(format == "bibtex"){
-      citations <- list()
-      for(reference in packages){
-        citations <- append(citations, citation(reference))
+  suppressWarnings(
+    if(missing(packages)){
+      packages <- c(packages, .packages())
+      if(include_r == FALSE){
+        if(format == "bibtex"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(toBibtex(citations))
+        }
+        if(format == "text"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(citations)
+        }
       }
-      print(toBibtex(citations))
-    }
-    if(format == "text"){
-      citations <- list()
-      for(reference in packages){
-        citations <- append(citations, citation(reference))
+      else{
+        packages <- append(packages, "base")
+        if(format == "bibtex"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(toBibtex(citations))
+        }
+        if(format == "text"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(citations)
+        }
       }
-      print(citations)
     }
-  }
-  else{
-    packages <- append(packages, "base")
-    if(format == "bibtex"){
-      citations <- list()
-      for(reference in packages){
-        citations <- append(citations, citation(reference))
+    else{
+      if(include_r == FALSE){
+        if(format == "bibtex"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(toBibtex(citations))
+        }
+        if(format == "text"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(citations)
+        }
       }
-      print(toBibtex(citations))
-    }
-    if(format == "text"){
-      citations <- list()
-      for(reference in packages){
-        citations <- append(citations, citation(reference))
-      }
-      print(citations)
-    }
-  })
+      else{
+        packages <- append(packages, "base")
+        if(format == "bibtex"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(toBibtex(citations))
+        }
+        if(format == "text"){
+          citations <- list()
+          for(reference in packages){
+            citations <- append(citations, citation(reference))
+          }
+          print(citations)
+        }
+      }}
+  )
 }
